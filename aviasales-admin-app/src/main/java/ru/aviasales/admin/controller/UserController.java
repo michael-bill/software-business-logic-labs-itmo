6c6103ef83dde8cadf16619e17aa6e59fb231028
Michael B. <biloshitskiy_m@mail.ru>
2025-02-26 17:37:17 +0300

    fix: create user

--- aviasales-admin-app/src/main/java/ru/aviasales/admin/controller/UserController.java
+++ aviasales-admin-app/src/main/java/ru/aviasales/admin/controller/UserController.java
@@ -3,12 +3,15 @@ package ru.aviasales.admin.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.aviasales.admin.dao.entity.User;
 import ru.aviasales.admin.dto.request.UserCreateReq;
 import ru.aviasales.admin.dto.response.UserResp;
+import ru.aviasales.admin.exception.NoPermissionException;
 import ru.aviasales.admin.service.auth.AuthService;
 
 @RestController
@@ -21,7 +24,13 @@ public class UserController {
 
     @Operation(summary = "Создание пользователя (только админам)")
     @PostMapping("/create")
-    public UserResp signUp(@RequestBody UserCreateReq request) {
+    public UserResp signUp(
+            @AuthenticationPrincipal User user,
+            @RequestBody UserCreateReq request
+    ) {
+        if (user.getRole() != User.Role.ADMIN) {
+            throw new NoPermissionException("Нет прав для создания пользователя");
+        }
         return authService.createUser(request);
     }
 
